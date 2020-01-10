9d2b950e7f9760575bb8cea103b5dd6654d69b1c
HxxxxxS <HxxxxxS@users.noreply.github.com>
2020-01-10 02:05:54 +0100

    Refactor

--- src/MyAddon/MyAddon.lua
+++ src/MyAddon/MyAddon.lua
@@ -5,13 +5,10 @@
 
 MyAddon = {}
 
-MyAddon_Name = "MyAddon"
-MyAddon_Version = "0"
-MyAddon_Title = MyAddon_Name.." v"..MyAddon_Version
-
-MyAddon_Enabled = 0;
-
-MyAddon_Colors = {
+MyAddon.Name = "MyAddon"
+MyAddon.Version = "1.0"
+MyAddon.Enabled = 0
+MyAddon.Colors = {
     red = "ffff0000",
     green = "ff00ff00",
     yellow = "ffffff00",
@@ -19,16 +16,19 @@ MyAddon_Colors = {
     orange = "ffe66600"
 }
 
-MyAddon_Cmds = {
+MyAddon.SlashCmd = "myaddon"
+MyAddon.Title = MyAddon.Name.." v"..MyAddon.Version
+
+MyAddon.Cmds = {
     {
         cmd = "on",
-        desc = "Enables "..MyAddon_Name,
+        desc = "Enables "..MyAddon.Name,
         cb = function() MyAddon:Enable() end
 
     },
     {
         cmd = "off",
-        desc = "Disables "..MyAddon_Name,
+        desc = "Disables "..MyAddon.Name,
         cb = function() MyAddon:Disable() end
     },
     {
@@ -41,12 +41,11 @@ MyAddon_Cmds = {
 function MyAddon:OnLoad()
 
     SLASH_MYADDON1 = "/myaddon"
-    SLASH_MYADDON2 = "/ma"
     SlashCmdList["MYADDON"] = function(msg)
         MyAddon:SlashCommandHandler(msg)
     end
 
-    MyAddon:Print(MyAddon:Color("green")..MyAddon_Title.." Loaded - /ma")
+    MyAddon:Print(MyAddon:Color("green")..MyAddon.Title.." Loaded - /"..MyAddon.SlashCmd)
 end
 
 function MyAddon:Print(msg)
@@ -54,7 +53,7 @@ function MyAddon:Print(msg)
 end
 
 function MyAddon:Color(col)
-    return "|c"..MyAddon_Colors[col]
+    return "|c"..MyAddon.Colors[col]
 end
 
 function MyAddon:SlashCommandHandler(msg)
@@ -63,7 +62,7 @@ function MyAddon:SlashCommandHandler(msg)
 
         local i = 0
 
-        for k,o in pairs(MyAddon_Cmds) do
+        for k,o in pairs(MyAddon.Cmds) do
             if command == o["cmd"] then
                 --MyAddon:Print(MyAddon:Color("green").."You triggered command '"..MyAddon:Color("yellow")..o["cmd"]..MyAddon:Color("green").."'")
                 if o.cb then
@@ -74,7 +73,7 @@ function MyAddon:SlashCommandHandler(msg)
             end
         end
         if i==0 then
-            MyAddon:Print(MyAddon:Color("yellow").."Unknown command " ..MyAddon:Color("green").."/ma "..msg);
+            MyAddon:Print(MyAddon:Color("yellow").."Unknown command " ..MyAddon:Color("green").."/"..MyAddon.SlashCmd.." "..msg);
             MyAddon:Help()
         end
     else
@@ -83,20 +82,20 @@ function MyAddon:SlashCommandHandler(msg)
 end
 
 function MyAddon:Help()
-    MyAddon:Print(MyAddon:Color("green")..MyAddon_Title..MyAddon:Color("yellow").." : Usage - /ma option");
-    for k,o in pairs(MyAddon_Cmds) do
-        MyAddon:Print(MyAddon:Color("green").."/ma "..MyAddon:Color("yellow")..o
+    MyAddon:Print(MyAddon:Color("green")..MyAddon.Title..MyAddon:Color("yellow").." : Usage - /"..MyAddon.SlashCmd.." option");
+    for k,o in pairs(MyAddon.Cmds) do
+        MyAddon:Print(MyAddon:Color("green").."/"..MyAddon.SlashCmd.." "..MyAddon:Color("yellow")..o
             ["cmd"]..MyAddon:Color("green")..MyAddon:Color("yellow")..MyAddon:Color("green")..": "..o["desc"])
     end
-    MyAddon:Print(MyAddon:Color("green").."Usage - /ma option");
+    MyAddon:Print(MyAddon:Color("green").."Usage - /"..MyAddon.SlashCmd.." option");
 end
 
 function MyAddon:Enable()
-    MyAddon_Enabled = 1
-    MyAddon:Print(MyAddon:Color("green")..MyAddon_Name.." enabled.")
+    MyAddon.Enabled = 1
+    MyAddon:Print(MyAddon:Color("green")..MyAddon.Name.." enabled.")
 end
 
 function MyAddon:Disable()
-    MyAddon_Enabled = 0
-    MyAddon:Print(MyAddon:Color("red")..MyAddon_Name.." disabled.")
+    MyAddon.Enabled = 0
+    MyAddon:Print(MyAddon:Color("red")..MyAddon.Name.." disabled.")
 end
